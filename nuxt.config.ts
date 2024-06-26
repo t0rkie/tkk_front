5221c6ee163cfa9d2abde22ce731e7cc8c211daa
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-06-27 03:36:27 +0900

    修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
-  nitro: {
-    preset: 'static',
-  },
+  // ssr: false,
+  // nitro: {
+  //   preset: 'static',
+  // },
 })
