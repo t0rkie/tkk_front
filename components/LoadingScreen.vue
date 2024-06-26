f73b4e8209872ad7e21da6c40c8b2a75919eb52c
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2024-06-26 15:21:28 +0900

    基本構成整える

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="{ fadeout: !isLoading }" class="loader">
+    <div class="splash">
+      TKK
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  isLoading: boolean
+}
+const props = defineProps<Props>()
+
+</script>
+<style scoped>
+.loader {
+  background-color: #D12424;
+  display: flex;
+  overflow: hidden;
+  position: fixed;
+  align-items: center;
+  text-align: center;
+  bottom: 0;
+  color: white;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+.splash {
+  
+}
+.fadeout {
+  animation: fadeout 2s forwards;
+}
+@keyframes fadeout {
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 参考 https://qiita.com/kokogento/items/fcd8b3fcc7f59e8bf887 -->
+  <LoadingScreen :isLoading="isLoading" />
+  <transition name="fade" v-if="!isLoading">
+    <slot />
+  </transition>
+</template>
+<script setup lang="ts">
+const isLoading = ref(true)
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
+</script>
